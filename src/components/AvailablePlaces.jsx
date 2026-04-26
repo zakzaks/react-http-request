09caa9b3fbe69981e7f91d11fa29d6768ce7ecc4
zakzaks <ariefn82@gmail.com>
2026-04-26 12:49:52 +0700

    first commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+
+	useEffect(() => {
+		async function fetchPlaces() {
+			const response = await fetch("http://localhost:3000/places");
+			const data = await response.json();
+			setAvailablePlaces(data.places);
+		}
+
+		fetchPlaces();
+	}, []);
+
+	return (
+		<Places
+			title="Available Places"
+			places={availablePlaces}
+			fallbackText="No places available."
+			onSelectPlace={onSelectPlace}
+		/>
+	);
+}
